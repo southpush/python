02e4d905a6209c0568590e485c1ee3ec484f9671
southpush <southpush@outlook.com>
2019-09-23 18:20:00 +0800

    complete exercise dnd-character

--- exercises/dnd-character/dnd_character.py
+++ exercises/dnd-character/dnd_character.py
@@ -1,3 +1,20 @@
+import random
+
+
+def modifier(num):
+    return int((num - 10) // 2)
+
+
 class Character:
     def __init__(self):
-        pass
+        self.strength = self.ability()
+        self.dexterity = self.ability()
+        self.constitution = self.ability()
+        self.intelligence = self.ability()
+        self.wisdom = self.ability()
+        self.charisma = self.ability()
+        self.hitpoints = 10 + modifier(self.constitution)
+
+    def ability(self):
+        results = [random.randint(1, 6) for i in range(4)]
+        return sum(results) - results.pop(random.randint(0, 3))
