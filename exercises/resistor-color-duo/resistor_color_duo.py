048191583ed2cf668c6180d851d100f277a74101
southpush <southpush@outlook.com>
2019-09-23 18:38:31 +0800

    complete exercise resistor-color-duo

--- exercises/resistor-color-duo/resistor_color_duo.py
+++ exercises/resistor-color-duo/resistor_color_duo.py
@@ -1,2 +1,16 @@
+color_dict = {
+    'black': 0,
+    'brown': 1,
+    'red': 2,
+    'orange': 3,
+    'yellow': 4,
+    'green': 5,
+    'blue': 6,
+    'violet': 7,
+    'grey': 8,
+    'white': 9,
+}
+
+
 def value(colors):
-    pass
+    return int(''.join(str(color_dict[i]) for i in colors[0: 2]))
